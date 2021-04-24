182c616923278ed51328e34e892fddf200c0f16b
aldrickdev <75044812+aldrickdev@users.noreply.github.com>
2021-04-24 00:16:53 -0400

    adds function to configure the api key

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import fastapi
 import uvicorn
+from pathlib import Path
+import json
+from services import openweather_service
 from starlette.staticfiles import StaticFiles
 from views import home
 from api import weather_api
@@ -8,6 +11,16 @@ api = fastapi.FastAPI()
 
 def configure():
   configure_routing()
+  configure_api_key()
+
+def configure_api_key():
+  file = Path('../settings.json').absolute()
+  if not file.exists():
+    print("settings.json not found in root of project")
+    raise Exception("settings.json not found in root of project")
+  with open('../settings.json') as fin:
+    settings = json.load(fin)
+    openweather_service.api_key = settings.get('api_key')
 
 def configure_routing():
   api.mount('/static', StaticFiles(directory='static'), name='static')
