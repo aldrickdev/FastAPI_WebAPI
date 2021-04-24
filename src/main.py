2ea1a52942eec98bdbbe2ec0a2d561de05dd233b
aldrickdev <75044812+aldrickdev@users.noreply.github.com>
2021-04-23 23:12:55 -0400

    orgianizes main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,21 @@
 import fastapi
 import uvicorn
-from starlette.requests import Request
-from starlette.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
+from views import home
+from api import weather_api
 
 api = fastapi.FastAPI()
-templates = Jinja2Templates('templates')
-api.mount('/static', StaticFiles(directory='static'), name='static')
 
-@api.get('/')
-def index(request :Request):
-  return templates.TemplateResponse('index.html', {'request' :request})
+def configure():
+  configure_routing()
+
+def configure_routing():
+  api.mount('/static', StaticFiles(directory='static'), name='static')
+  api.include_router(home.router)
+  api.include_router(weather_api.router)
 
 if __name__ == '__main__':
+  configure()
   uvicorn.run(api, port=8001, host='127.0.0.1')
+else:
+  configure()
